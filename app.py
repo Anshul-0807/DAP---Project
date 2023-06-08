2ad836d631804273795806e5252c0a56b9f4d288
Anshul patle <111644282+Anshul-0807@users.noreply.github.com>
2023-06-08 15:40:42 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 import streamlit as st
 import pandas as pd
 
-st.sidebar.radio(
+import preprocessor, helper
+
+df = preprocessor.preprocess()
+
+user_menu = st.sidebar.radio(
     'select an option ',
     ('Medal Tally', 'Overall Analysis', 'Country-wise Analysis', 'Athlete-wise Analysis')
 )
+
+st.dataframe(df)
+
+if user_menu == 'Medal Tally':
+    Medal_tally = helper.Medal_tally(df)
+    st.dataframe(Medal_tally)
 

5743a2650f1cc8fa95c95675683ef36c5ac4e3ee
Anshul patle <111644282+Anshul-0807@users.noreply.github.com>
2023-06-09 19:26:05 +0530

    Add files via upload

--- preprocessor.py
+++ preprocessor.py
@@ -1,14 +1,9 @@
 import pandas as pd
-
-df = pd.read_csv('athlete_events.csv')
-region_df = pd.read_csv('noc_regions.csv')
-
-def preprocess():
-    global df, region_df
+def preprocess(df, region_df):
     # filtering for summer olympic
     df = df[df['Season'] == 'Summer']
     # merge with region_df
-    df = df.merge(region_df, on='NOC' , how='left')
+    df = df.merge(region_df, on='NOC', how='left')
     # drop duplicates
     df.drop_duplicates(inplace=True)
     # one hot encoding medals
